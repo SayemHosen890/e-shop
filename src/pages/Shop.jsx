13e090ae2e8132a0f042b1667069a16873b7ae18
SayemHosen890 <sayemhosen902@gmail.com>
2025-09-07 17:57:43 +0600

    fixed-col

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,7 +5,7 @@ import ProductCard from '../components/ProductCard';
 const Shop = () => {
     const products=useSelector(state=>state.product);
   return (
-    <div className=" mx-auto !mx-22 py-12 px-6 md:px-16 lg:px-24">
+    <div className="container mx-auto py-12 md:px-16 lg:px-24">
         <h2 className="text-2xl text-center font-bold mb-6">Shops</h2>
         <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-5 gap-6 cursor-pointer">
           {products.products.map((product,index)=>(
